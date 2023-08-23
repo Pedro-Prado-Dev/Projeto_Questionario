c029e76bcc32ec910abb32257ce561846aa558a5
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2023-08-22 22:45:48 -0300

    Starting to create the quiz

--- project_quiz/home/views.py
+++ project_quiz/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from question.models import Quest
 
 # Create your views here.
 def home(request):
-    return render(request,'home_page/index.html')
+    quest = {
+        'quest': Quest.objects.all()
+    }
+    print(Quest.objects.all())
+    return render(request,'home_page/index.html', quest)
+
